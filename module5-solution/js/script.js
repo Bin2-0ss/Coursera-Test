9ce741ea271747d6f03716d1a0ab5949ba4725e6
Bin2-0ss <Bin2-0ss>
2020-06-07 22:19:59 +0530

    second 2 commit

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  //alert('CS: in the function buildAndShowHomeHTML');
+  alert('CS: in the function buildAndShowHomeHTML');
   alert(homeHtmlUrl);
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
